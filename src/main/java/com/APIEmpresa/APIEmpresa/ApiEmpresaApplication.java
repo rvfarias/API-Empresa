244c2852e41fb53c82a99aca71c4f83ab727b25b
Rafael <rafaelvlfarias@gmail.com>
2022-06-28 16:42:37 -0300

    commit inical

---
+++ src/main/java/com/APIEmpresa/APIEmpresa/ApiEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.APIEmpresa.APIEmpresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmpresaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/APIEmpresa/APIEmpresa/ApiEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APIEmpresa.APIEmpresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
